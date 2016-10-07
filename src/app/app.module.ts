588b520d139a819fa3a94c1aa93267a743356f18
cwutthipong <c.wutthipong@gmail.com>
2016-10-07 11:53:52 +0700

    Create Page and Change tab name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { Menu } from '../pages/menu/menu';
+import { Order } from '../pages/order/order';
+import { Report } from '../pages/report/report';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    Menu,
+    Order,
+    Report
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -23,7 +29,10 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    Menu,
+    Order,
+    Report
   ],
   providers: []
 })
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
-import { HomePage } from '../home/home';
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { Menu } from '../menu/menu';
+import { Order } from '../order/order';
+import { Report } from '../report/report';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,11 +10,12 @@ import { ContactPage } from '../contact/contact';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
-  tab3Root: any = ContactPage;
+  tab1Root: any = Menu;
+  tab2Root: any = Order;
+  tab3Root: any = Report;
 
   constructor() {
 
   }
 }
+
